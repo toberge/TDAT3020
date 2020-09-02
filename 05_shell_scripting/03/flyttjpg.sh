de053e401c280e05528f9207a222a4106f5e7b76
toberge <toberge@stud.ntnu.no>
2020-09-02 19:31:27 +0200

    øv5: Fix the actual bug in problem 3

--- 05_shell_scripting/03/flyttjpg.sh
+++ 05_shell_scripting/03/flyttjpg.sh
@@ -3,6 +3,10 @@
 
 # NB: Ingen *feil* i denne fila, men en del steder der " lønner seg
 
+# Alternativ 2: Gjør om flyttjpg.sh
+# Hvis katalognavnet har en /jpg til sist fra før, avbryt
+[ "${1%%/jpg/*.jpg}" = "$1" ] || exit
+
 # Beregn katalognavn utfra filnavn
 JPGDIR="$(dirname "$1")"/jpg
 
--- 05_shell_scripting/03/rydd.sh
+++ 05_shell_scripting/03/rydd.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Feil nr. 1:
 # Denne find-kommandoen kjører `flyttjpg` med *alle* filene som argument
 # – siden `flyttjpg` kun bruker argument nr. 1 vil ikke dette fungere
 #   (kun én fil vil håndteres om gangen)
@@ -10,5 +11,14 @@
 # - bruke xargs for å gi kun *ett* argument videre
 # - skrive om flyttjpg så den benytter seg av hele argumentlista
 
-# Velger det første:
-find /home -type f -name "*.jpg" -print0 | xargs -0 -n1 flyttjpg
+# Feil nr. 2:
+# Den faktiske feilen at det lages stadig nye jpg-mapper hver gang skriptet kjøres.
+# En vil sitte igjen med en ubrukelig stor mengde unødvendige undermapper.
+
+# Har igjen noen alternativer
+# - få xargs til å ekskludere alle mapper kalt jpg
+# - avbryte skriptet om filbana slutter på /jpg/*.jpg
+
+# Velger å håndtere begge deler i xargs:
+find /home -type f -name "*.jpg" -not -path "*/jpg/*" -print0 \
+    | xargs -0 -n1 flyttjpg
