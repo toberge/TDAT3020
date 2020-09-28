e36c00998ce7b3b4d98e14a23b8c48680553ad42
Tore Bergebakken <toberge@stud.ntnu.no>
2020-09-28 16:58:19 +0200

    øv12: barcode done

---
+++ 12_ctf2/pos/README.md
@@ -0,0 +1,3 @@
+Finding a possible mysql injection was rather simple (it isn't even in a string), and here it is, visible right in the barcode:
+
+![barcode](barcode.svg)
---
+++ 12_ctf2/pos/setup.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE myshop;
+USE myshop;
+
+CREATE TABLE `products` (
+  `id` int(11) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `products` VALUES (1554949,20),(1554950,25),(1554952,15),(1554953,25);
